50d459f0e56e37c0e5c3d37a28cda05266449979
saidurga08 <saidurgasajith@gmail.com>
2025-03-27 00:42:41 +0530

    Made a few changes with receiving form values

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseForm = document.getElementById("expense-form");
   const expenseName = document.getElementById("name");
   const expenseAmount = document.getElementById("amount");
-  const expenseList = document.getElementById("expense-list");
-  const resultDisplay = document.getElementById("result");
-
   let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
   let total = calc();
 });
