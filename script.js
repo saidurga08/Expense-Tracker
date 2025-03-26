7221ee42688774ca2f8651170f2428a7f481e5e8
saidurga08 <saidurgasajith@gmail.com>
2025-03-27 00:43:49 +0530

    Made new changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseForm = document.getElementById("expense-form");
   const expenseName = document.getElementById("name");
   const expenseAmount = document.getElementById("amount");
+
   let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
   let total = calc();
 });
